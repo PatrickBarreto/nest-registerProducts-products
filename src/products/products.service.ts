dec3d35600f0274a5eeefcfbf345640ba0493e1b
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-09-03 14:34:25 -0300

    Create product services to be used by controller

---
+++ src/products/products.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsRpository } from './repository/products.repository'; 
+
+@Injectable()
+export class ProductsService {
+  constructor(private readonly repository: ProductsRpository){
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.repository.create(createProductDto);
+  }
+
+  findAll() {
+    return this.repository.findAll();
+  }
+
+  findOne(id: number) {
+    return this.repository.findOne(id);
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return this.repository.update(id, updateProductDto);
+  }
+
+  remove(id: number) {
+    return this.repository.delete(id);
+  }
+}
