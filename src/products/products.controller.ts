206fd68ed924176a1e6dc21ee032717b9d8e4684
Patrick Barreto <contato.patrickbarreto@gmail.com>
2024-09-03 14:35:22 -0300

    Create product controller to handler http routes, call services and prepare response data from one request

---
+++ src/products/products.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, HttpCode } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @HttpCode(201)
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+  
+  @HttpCode(200)
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+  
+  @HttpCode(200)
+  @Get(':id')
+  findOne(@Param('id', new ParseIntPipe()) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @HttpCode(200)
+  @Patch(':id')
+  update(@Param('id', new ParseIntPipe()) id: number, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(id, updateProductDto);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id', new ParseIntPipe()) id: number) {
+    return this.productsService.remove(id);
+  }
+}
